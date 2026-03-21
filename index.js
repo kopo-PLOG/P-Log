f7f762d38199480eaa6f428efe447376b6b1c2fa
yangtori0407 <heyang236@gmail.com>
2026-03-21 21:19:08 +0900

    feat: #9 연속 키보드 눌림 방지

--- index.js
+++ index.js
@@ -1,84 +1,100 @@
 const state = [
     {
         id: "base",
-        src: "image/indexImg/pico_base.gif",
+        src: "./image/indexImg/pico_base.gif",
         alt: "피코 평범 버전",
-        time: 10000
+        time: 10000,
+        num: 0
     },
     {
         id: "food",
-        src: "image/indexImg/pico_hungry.gif",
+        src: "./image/indexImg/pico_hungry.gif",
         alt: "피코 배고픈 버전",
-        time: 10000
+        time: 10000,
+        num: 2
     },
     {
         id: "toilet",
-        src: "image/indexImg/pico_toilet.gif",
+        src: "./image/indexImg/pico_toilet.gif",
         alt: "피코 화장실 버전",
-        time: 10000
+        time: 10000,
+        num: 2
     },
     {
         id: "game",
-        src: "image/indexImg/pico_bored.gif",
+        src: "./image/indexImg/pico_bored.gif",
         alt: "피코 심심한 버전",
-        time: 10000
+        time: 10000,
+        num: 3
     },
     {
         id: "angry",
-        src: "image/indexImg/pico_angry.gif",
+        src: "./image/indexImg/pico_angry.gif",
         alt: "피코 화난 버전",
-        time: 5000
+        time: 5000,
+        num: 4
     },
     {
         id: "clear",
-        src : "image/indexImg/pico_clear.gif",
+        src : "./image/indexImg/pico_clear.gif",
         alt : "피코 해결 후 버전",
-        time : 3000
+        time : 3000,
+        num: 5
     },
     {
         id : "adult",
-        src : "image/indexImg/pico_adult.gif",
+        src : "./image/indexImg/pico_adult.gif",
         alt : "피코 성장 후 버전",
-        time : 5000
+        time : 5000,
+        num: 6
     },
     {
         id : "changing",
-        src : "image/indexImg/pico_chaging.gif",
+        src : "./image/indexImg/pico_changing.gif",
         alt : "피코 변신 버전",
-        time : 5000
+        time : 3000,
+        num: 7
     },
     {
         id : "tired",
-        src : "image/indexImg/pico_tired.gif",
+        src : "./image/indexImg/pico_tired.gif",
         alt : "피코 피곤 버전",
-        time : 10000
+        time : 10000,
+        num: 8
+    },
+    {
+        id : "wonder",
+        src : "./image/indexImg/pico_wonder.jpg",
+        alt : "피코 물음표 버전",
+        time : 2000,
+        num: 9
     }
 ]
 
 const health = [
     {
         id : "heart_1",
-        src : "image/indexImg/heart_1.png",
+        src : "./image/indexImg/heart_1.png",
         alt : "heart_1"
     },
     {
         id : "heart_2",
-        src : "image/indexImg/heart_2.png",
+        src : "./image/indexImg/heart_2.png",
         alt : "heart_2"
     },
     {
         id : "heart_3",
-        src : "image/indexImg/heart_3.png",
+        src : "./image/indexImg/heart_3.png",
         alt : "heart_3"
     },
     {
         id : "heart_4",
-        src : "image/indexImg/heart_4.png",
+        src : "./image/indexImg/heart_4.png",
         alt : "heart_4"
     },
     {
         id : "heart_5",
-        src : "image/indexImg/heart_5.png",
+        src : "./image/indexImg/heart_5.png",
         alt : "heart_5"
     }
 ]
@@ -86,27 +102,27 @@ const health = [
 const level = [
     {
         id : "gage_1",
-        src : "image/indexImg/gage_1.png",
+        src : "./image/indexImg/gage_1.png",
         alt : "gage_1"
     },
     {
         id : "gage_2",
-        src : "image/indexImg/gage_2.png",
+        src : "./image/indexImg/gage_2.png",
         alt : "gage_2"
     },
     {
         id : "gage_3",
-        src : "image/indexImg/gage_3.png",
+        src : "./image/indexImg/gage_3.png",
         alt : "gage_3"
     },
     {
         id : "gage_4",
-        src : "image/indexImg/gage_4.png",
+        src : "./image/indexImg/gage_4.png",
         alt : "gage_4"
     },
     {
         id : "gage_5",
-        src : "image/indexImg/gage_5.png",
+        src : "./image/indexImg/gage_5.png",
         alt : "gage_5"
     },
 ]
@@ -127,17 +143,32 @@ if(currentHealth ==  null){
     localStorage.setItem("health", 80);
 }
 
-let lastState = 1;
-localStorage.setItem("lastState", lastState);
 
 ///////////////////     전환      //////////////////////////
+
+//전에 행동과 중복되는 걸 방지하기 위해 저장
+let lastState = 1;
+localStorage.setItem("lastState", lastState);
 
 const main = document.getElementById("mainImg");
 let randomState = 0;
 let angryTimer = null;
+let baseTimer = null;
+let wonderTimer = null;
+
+//중복 방지 flag값
+let wrongState = 0;
+let rightState = 0;
+
+//현재가 요구사항이 있는 상황인지 아닌지 저장
+let currentState = "";
+
+const levelNum = parseInt(localStorage.getItem("level"));
 
 ////start
-startBase();
+if(levelNum != 120){
+    startBase();
+}
 
 drawHealth();
 drawLevel()
@@ -196,11 +227,16 @@ function drawHealth(){
 //level 게이지
 function drawLevel(){
     const levelGage = parseInt(localStorage.getItem("level"));
+    const healthGage = parseInt(localStorage.getItem("health"));
     const gage = document.getElementById("gage").firstElementChild;
 
     const gageNum = document.getElementById("gageNum").firstElementChild;
     gageNum.innerText = `${levelGage}/120`
-    if(levelGage === 120){
+
+    //health + levelGage로 할까말까
+    if(levelGage === 120 && healthGage > 40){
+
+        gage.src = level[4].src;
         main.src = state[7].src;
 
         setTimeout(() => {
@@ -214,7 +250,7 @@ function drawLevel(){
         gage.src = level[2].src
     }else if(levelGage > 60 && levelGage <= 90){
         gage.src = level[3].src;
-    }else if(levelGage > 90 && levelGage < 120){
+    }else if(levelGage > 90 && levelGage <= 120){
         gage.src = level[4].src;
     }
 }
@@ -225,11 +261,16 @@ function startBase(){
     const gage = parseInt(localStorage.getItem("health"));
     console.log(`statBase() : health = ${gage}`);
 
+    currentState = "normal";
+
     if(gage <= 40){
         main.src = state[8].src;
     }else{
         main.src = state[0].src;
     }
+
+    wrongState = 0;
+    rightState = 0;
 
     setTimeout(() => {
         //console.log("dkdkdkdkdkdkdk")
@@ -239,10 +280,9 @@ function startBase(){
 
 function randomEvent(){
 
-    //이전 타이머가 돌고 있다면 청소
-    if(angryTimer) {
-        clearTimeout(angryTimer);
-    }
+    // 모든 종류의 타이머를 먼저 초기화
+    if(angryTimer) clearTimeout(angryTimer);
+    if(baseTimer) clearTimeout(baseTimer);
 
     while(true){
         randomState = Math.floor(Math.random() * 3) + 1;
@@ -257,7 +297,7 @@ function randomEvent(){
         }
     }
     main.src = state[randomState].src;
-
+    currentState = state[randomState].id
     angryTimer = setTimeout(() => {
         console.log("반응 못함");
         getAngry();
@@ -278,7 +318,7 @@ function getAngry(){
     
     drawHealth();
 
-    setTimeout(() => {
+    baseTimer = setTimeout(() => {
         startBase();
     }, state[4].time);
 }
@@ -317,16 +357,7 @@ document.addEventListener("keydown", (e) => {
             game.classList.add("check");
         }
     }
-    // }else if(e.key === "ArrowDown"){
-    //     //console.log("하측");
-    //     if(game.classList.contains("check")){
-    //         console.log("game 선택");
-    //     }else if(toilet.classList.contains("check")){
-    //         console.log("toilet 선택");
-    //     }else if(food.classList.contains("check")){
-    //         console.log("food 선택");
-    //     }
-    // }
+
 
 
     //행동에 대한 키보드 이벤트
@@ -335,8 +366,25 @@ document.addEventListener("keydown", (e) => {
     if(e.key === "ArrowDown"){
         console.log("select 이벤트! ArrowDown")
         console.log(checkState[0].id);
-        console.log(state[randomState].id)
-        if(checkState[0].id === state[randomState].id){
+        console.log(state[randomState].id);
+
+        if(currentState === "normal"){
+            console.log("wonder!")
+            //예약된 setTimeout() 함수 취소
+            clearTimeout(angryTimer);
+            //clearTimeout(baseTimer);
+            angryTimer = null;
+
+            main.src = state[9].src;
+
+            if(wonderTimer) return;
+
+            wonderTimer = setTimeout(() => {
+                startBase();
+            }, state[5].time);
+
+        }else if(checkState[0].id === state[randomState].id){
+
 
             //요구에 맞는 행동 선택
             if(angryTimer){
@@ -350,20 +398,30 @@ document.addEventListener("keydown", (e) => {
                 
                 //예약된 setTimeout() 함수 취소
                 clearTimeout(angryTimer);
+                //clearTimeout(baseTimer);
                 angryTimer = "";
 
                 main.src = state[5].src;
-                setTimeout(() => {
+                if(rightState === 0){
+                    rightState = 1;
+                    setTimeout(() => {
                     startBase();
                 }, state[5].time);
+                }
+                
             }
         }else{
 
             // //요구에 잘못된 행동 선택
             console.log("잘못 선택했습니다!!!")
+            
             clearTimeout(angryTimer);
             angryTimer = "";
-            getAngry();
+            if(wrongState === 0){
+                getAngry();
+                wrongState = 1;
+            }
+            
         }
     }
 })
